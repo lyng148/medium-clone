123c528f837f45326c589669bb0c8c28d5503610
Your Name <your.email@example.com>
2025-07-16 21:17:14 +0700

    feat(article): add favorite and unfavorite functionality for articles

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
-import { ArticlesService } from './articles.service';
-import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
-import { CurrentUser } from 'src/common/decorators/user.decorator';
-import { User } from 'generated/prisma';
-import { ArticleOwnerGuard } from 'src/common/guards/article-owner.guard';
-import { Public } from 'src/auth/constants';
+import {Controller, Get, Post, Body, Param, Delete, Put, UseGuards} from '@nestjs/common';
+import {ArticlesService} from './articles.service';
+import {CreateArticleDto} from './dto/create-article.dto';
+import {UpdateArticleDto} from './dto/update-article.dto';
+import {CurrentUser} from 'src/common/decorators/user.decorator';
+import {User} from 'generated/prisma';
+import {ArticleOwnerGuard} from 'src/common/guards/article-owner.guard';
+import {Public} from 'src/auth/constants';
 
 @Controller('articles')
 export class ArticlesController {
-  constructor(private readonly articlesService: ArticlesService) {}
+  constructor(private readonly articlesService: ArticlesService) { }
 
   @Post()
   create(@CurrentUser() currUser: User, @Body() createArticleDto: CreateArticleDto) {
@@ -36,5 +36,15 @@ export class ArticlesController {
   @UseGuards(ArticleOwnerGuard)
   remove(@Param('slug') slug: string) {
     return this.articlesService.remove(slug);
+  }
+
+  @Post(':slug/favorite')
+  favoriteArticle(@CurrentUser() currUser: User, @Param('slug') slug: string) {
+    return this.articlesService.favoriteArticle(currUser, slug);
+  }
+
+  @Delete(':slug/favorite')
+  unfavoriteArticle(@CurrentUser() currUser: User, @Param('slug') slug: string) {
+    return this.articlesService.unfavoriteArticle(currUser, slug);
   }
 }
