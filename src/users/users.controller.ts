2bc82e23784778b11a878a4c0473fe59e8b068a4
Your Name <your.email@example.com>
2025-07-17 10:53:30 +0700

    feat(users): implement follow and unfollow user functionality with profile retrieval

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { Public } from 'src/auth/constants';
@@ -8,32 +8,48 @@ import { CurrentUser } from 'src/common/decorators/user.decorator';
 import { UpdateUserValidationPipe } from 'src/common/pipes/update-user.pipe';
 import { UpdateUserDto } from './dtos/update-user.dto';
 
-@Controller('users')
+@Controller()
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Public()
-  @Post()
+  @Post('users')
   async register(@Body() createUserDTO: CreateUserDTO) {
     return this.userService.createUser(createUserDTO);
   }
 
   @Public()
-  @Post('login')
+  @Post('users/login')
   async login(@Body() loginUserDTO: LoginUserDTO) {
     return this.userService.loginUser(loginUserDTO);
   }
 
-  @Get()
+  @Get('user')
   getCurrentUser(@CurrentUser() user: User) {
     return this.userService.getCurrentUser(user);
   }
 
-  @Put()
+  @Put('user')
   updateUser(
     @CurrentUser() user: User,
     @Body(new UpdateUserValidationPipe()) updateUserDTO: UpdateUserDto,
   ) {
     return this.userService.updateUser(user, updateUserDTO);
+  }
+
+  @Public()
+  @Get('profiles/:username')
+  async getProfile(@Param('username') username: string, @CurrentUser() currentUser?: User) {
+    return this.userService.getProfile(username, currentUser);
+  }
+
+  @Post('profiles/:username/follow')
+  async followUser(@CurrentUser() currUser: User, @Param('username') username: string) {
+    return this.userService.followUser(currUser, username);
+  }
+
+  @Delete('profiles/:username/follow')
+  async unfollowUser(@CurrentUser() currUser: User, @Param('username') username: string) {
+    return this.userService.unfollowUser(currUser, username);
   }
 }
